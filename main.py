e046893b74fbb97a48eadbcce2f735aacfbff40e
willdc4 <williamdavicoelho@gmail.com>
2019-11-05 15:16:35 -0300

    leitura e distancia euclidiana

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+
+def read_database_file(file_name):
+    try:
+        database_file = open(file_name,'r')
+        row_list = []
+        for row in database_file:
+            row = row[:-1].split(',')
+            row_list.append(row)
+        database_file.close()
+        return row_list
+    except:
+        sys.exit('Error, verify that the file exists')
+
+def euclidean_distance(database_instance, test_data, number_of_attributes):
+    distance = 0
+    for i in range(number_of_attributes):
+        distance += (float(database_instance[i]) - float(test_data[i])) ** 2
+    return distance
+
+#def knn(k, database, test_data):
+    #for instance in database:
+
+
+data = read_database_file(sys.argv[1])
+print('Instance 1:',data[0][0], data[0][1])
+print('Instance 2:',data[1][0], data[1][1])
+print('Distance:',euclidean_distance(data[0],data[1], 2))
